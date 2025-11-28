915ff44b006e6a5620703c08808ef154adbdf7fc
aykutdanisman <aykutdanisman@gmail.com>
2025-11-28 16:25:19 +0000

    CCT-2415 merge to main (#48)
    * CCT-2415 merge to main
    
    * Update coredomain.version to 17.103.10
    
    ---------
    
    Co-authored-by: jayenhc <jayenhc@yahoo.com>

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -2,9 +2,9 @@
 
 CONTEXT_NAME=stagingpubhub
 
-FRAMEWORK_LIBRARIES_VERSION=17.101.2
-FRAMEWORK_VERSION=17.101.6
-EVENT_STORE_VERSION=17.101.5
+FRAMEWORK_LIBRARIES_VERSION=$(mvn help:evaluate -Dexpression=framework-libraries.version -q -DforceStdout)
+FRAMEWORK_VERSION=$(mvn help:evaluate -Dexpression=framework.version -q -DforceStdout)
+EVENT_STORE_VERSION=$(mvn help:evaluate -Dexpression=event-store.version -q -DforceStdout)
 
 DOCKER_CONTAINER_REGISTRY_HOST_NAME=crmdvrepo01
 
